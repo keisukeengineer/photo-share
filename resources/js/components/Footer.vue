573a79fd9b8ffbef8cef82c4d060c8a1eb5cdf94
keisukeengineer <nkmtear@yahoo.co.jp>
2020-08-26 22:25:45 +0900

    ログアウトボタン削除

--- resources/js/components/Footer.vue
+++ resources/js/components/Footer.vue
@@ -1,11 +1,5 @@
 <template>
   <footer class="footer">
-    <button v-if="isLogin" class="button button--link" @click="logout">
-      Logout
-    </button>
-    <!-- <RouterLink v-else class="button button--link" to="/login">
-      Login / Register
-    </RouterLink> -->
     <div class="share_wrapper">
       <div
         class="share_wrapper_sub cursor"
@@ -25,25 +19,8 @@
 </template>
 
 <script>
-import { mapState, mapGetters } from 'vuex'
-
 export default {
-  computed: {
-    ...mapState({
-      apiStatus: state => state.auth.apiStatus
-    }),
-    ...mapGetters({
-      isLogin: 'auth/check'
-    })
-  },
   methods: {
-    async logout () {
-      await this.$store.dispatch('auth/logout')
-
-      if (this.apiStatus) {
-        this.$router.push('/login')
-      }
-    },
     onShareClick(shareButton) {
       if(shareButton === 'twitter') {
         this.twitter()

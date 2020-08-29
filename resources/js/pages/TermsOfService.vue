8306431277ab20122d5ab3a079232c97b9ecba38
keisukeengineer <nkmtear@yahoo.co.jp>
2020-08-29 17:34:16 +0900

    文言追記

--- resources/js/pages/TermsOfService.vue
+++ resources/js/pages/TermsOfService.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="terms-of-service">
-    <h1 id="theme">Photo Share Terms of Use</h1>
+    <h1 id="theme">Terms of Service</h1>
     <div id="terms-of-service_first_contents">
       Regarding the use of Photo Share, the following terms of use are established.
     </div>
@@ -10,7 +10,7 @@
     </div>
     <h2 class="item">2. Resister</h2>
     <div class="item_contents">
-      We may differ in the services provided in Tabelog from customers who are not 
+      We may differ in the services provided in Photo Share from customers who are not 
       Photo Share members or who have not completed the member registration procedures prescribed by us.
     </div>
     <h2 class="item">3. Security</h2>
@@ -76,7 +76,7 @@
       <div class="privacy-item">
         <div class="privacy-item-theme">[5] Other</div>
         <div class="privacy-item_contents">
-          Please understand that if you do not fill in all the items, all or part of Photo Share services 
+          Please understand that if you do not fill in all the items, all or part of Photo Share 
           may not be available.
         </div>
       </div>
@@ -87,7 +87,7 @@
       <div class="use-item">
         <div class="use-item-theme">[1] Prohibition of diversion/resale</div>
         <div class="use-item_contents">
-          The customer shall not use or access Photo Share services provided by the Company, in whole or in part, 
+          The customer shall not use or access Photo Share provided by the Company, in whole or in part, 
           for the purpose of business activities or other profit-promoting activities or activities equivalent 
           thereto or preparation activities therefor. I shall. In addition, we shall not use or access for 
           the purpose of religious activities or political activities.
@@ -100,7 +100,7 @@
       <div class="use-item">
         <div class="use-item-theme">[2] Change / stop</div>
         <div class="use-item_contents">
-          We may change the tabelog provided to our customers for our convenience. Please note that Photo Share services 
+          We may change the Photo Share provided to our customers for our convenience. Please note that Photo Share services 
           may be stopped in the event of a disaster, accident, or other emergency. In addition, even if the customer 
           or a third party is damaged by changing or stopping the eating log without obtaining the customer's consent 
           in advance, we will not compensate at all.
@@ -109,7 +109,7 @@
       <div class="use-item">
         <div class="use-item-theme">[3] Equipment required for use</div>
         <div class="use-item_contents">
-          In order for customers to use Photo Share services, it is necessary to access the web. Regarding access to the web, 
+          In order for customers to use Photo Share, it is necessary to access the web. Regarding access to the web, 
           the customer is required to properly prepare and operate the necessary equipment and software at 
           his own responsibility and cost. We are not involved at all.
         </div>
@@ -121,11 +121,97 @@
       any damages caused to customers by the information on the posted photos or troubles between customers.
     </div>
     <h2 class="item">8. Right</h2>
-    <div class="item_contents">nothing yet</div>
+    <div class="item_contents">
+      <div class="right-item_contents">
+        All copyrights, trademark rights, other intellectual property rights, and other property rights relating to 
+        the contents and individual information, trademarks, images, advertisements, designs, etc. contained in 
+        Photo Share belong to the Company or a legitimate right holder. I will.
+      </div>
+      <div class="right-item_contents">
+        Photo Share and all related software include property rights protected by laws related to intellectual property rights.
+      </div>
+      <div class="right-item_contents">
+        You may use or use the software if it is licensed to be used or used by the Company or a third party who has 
+        copyright or other intellectual property rights or other property rights, or without the permission of 
+        the copyright holder by law. Except where permitted, Photo Share and Photo Share contents shall not be duplicated, 
+        edited, altered, posted, reprinted, publicly transmitted, distributed, sold, provided, translated, adapted, 
+        or otherwise used or used.
+      </div>
+    </div>
     <h2 class="item">9. Prohibited matter</h2>
-    <div class="item_contents">nothing yet</div>
+    <div class="item_contents">
+      <div class="prohibit-item_contents">
+        It is prohibited for the customer to perform the following actions when using Photo Share.
+      </div>
+      <div class="prohibit-item_contents">
+        Unless authorized by law or in accordance with this agreement or guideline, the information 
+        provided by Photo Share may be reproduced, reproduced, duplicated, sent, transferred, distributed, 
+        distributed, or copied by other methods without prior consent of the Company. Resale or keep for use for 
+        these purposes
+      </div>
+      <div class="prohibit-item_contents">
+        Violate these Terms or Guidelines
+      </div>
+      <div class="prohibit-item_contents">
+        Offensive to public order and morals
+      </div>
+      <div class="prohibit-item_contents">
+        Linking to or promoting illegal or criminal or seriously dangerous behavior
+      </div>
+      <div class="prohibit-item_contents">
+        Infringe on the intellectual property rights of the Company, other customers or third parties 
+        (including but not limited to copyright, design right, utility model right, trademark right, patent right, 
+        and know-how).
+      </div>
+      <div class="prohibit-item_contents">
+        Illegal or may infringe the rights or interests of other customers or third parties
+      </div>
+      <div class="prohibit-item_contents">
+        Interfering with the operation of Photo Share or damaging our credit
+      </div>
+      <div class="prohibit-item_contents">
+        False declaration when registering for Photo Share
+      </div>
+      <div class="prohibit-item_contents">
+        The same person can register multiple Photo Share members
+      </div>
+      <div class="prohibit-item_contents">
+        Use Photo Share function for the purpose of encountering unfamiliar opposite sex
+      </div>
+      <div class="prohibit-item_contents">
+        Other things that we judge to be inappropriate
+      </div>
+      <div class="prohibit-item_contents">
+        In the event that the customer conducts an action that falls under any of the items in 
+        the preceding paragraph or an action that the Company deems inappropriate in view of 
+        the purpose and purpose of the Photo Share, the Company will stop the use of the Photo Share for the customer or 
+        other May take appropriate measures (hereinafter referred to as "suspension measures"). In addition, 
+        the suspension measures shall be based on the discretion and judgment of the Company regardless of whether 
+        the customer is responsible or not. We will not be able to give any answer to the customer regardless of 
+        the reason why we took the suspension measure. In addition, regarding the damage caused to the customer due to 
+        the suspension measure, We do not take any responsibility.
+      </div>
+    </div>
     <h2 class="item">10. Unsubscribed</h2>
-    <div class="item_contents">nothing yet</div>
+    <div class="item_contents">
+      <div class="unsubscribed-item_contents">
+        If a Photo Share member wishes to withdraw from the membership, he/she will perform the prescribed withdrawal procedure.
+      </div>
+      <div class="unsubscribed-item_contents">
+        Photo Share members will lose all rights and privileges related to their Photo Share members' accounts when 
+        they withdraw from membership.
+      </div>
+      <div class="unsubscribed-item_contents">
+        Photo Share members agree in advance that the content of reviews posted by themselves will not be deleted even if 
+        the withdrawal procedure is performed.
+      </div>
+      <div class="unsubscribed-item_contents">
+        3. Even if the Photo Share member completes the withdrawal procedure, or if the contract between the Company and 
+        Photo Share member ends under this agreement, 4. Registration and management of login accounts, etc. 
+        5. Regarding privacy, 6. Use of Photo Share (1), (2), 7. Disclaimer, 8. Copyrights, property rights and 
+        other rights, 9. Prohibited acts, 12. Governing law and jurisdiction are still valid.
+      </div>
+    </div>
     <h2 class="item">11. Contact Us</h2>
     <div class="item_contents">
       If you have any questions, please contact us using the form&nbsp;
@@ -149,11 +235,8 @@
     }
   }
 
-  &_first_contents {
-    margin-bottom: 2rem;
-  }
-
   #theme {
+    text-align: center;
     padding: .5rem 0;
     margin-bottom: 2.5rem;
     border-top: 1rem solid rgb(175, 169, 169);
@@ -163,6 +246,11 @@
       font-size: 1.8rem;
     }
   }
+
+  &_first_contents {
+    margin-bottom: 2rem;
+  }
+
   .item {
     @media screen and (max-width: 375px) {
       font-size: 1.3rem;
@@ -172,7 +260,11 @@
       padding-left: 1rem;
       padding-bottom: 1rem;
 
-      .privacy-item {
+      .privacy-item,
+      .use-item,
+      .right-item,
+      .prohibit-item,
+      .unsubscribed-item {
         padding-bottom: 1rem;
 
         &-theme {
@@ -181,18 +273,6 @@
 
         &_first_contents {
           padding-bottom: 1rem;
-        }
-
-        &_contents {
-          padding-top: 1rem;
-        }
-      }
-
-      .use-item {
-        padding-bottom: 1rem;
-
-        &-theme {
-          font-weight: bold;
         }
 
         &_contents {

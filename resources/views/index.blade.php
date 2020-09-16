65f61ef0efeeb0e894d276cbe56953dfb0c76e46
keisukeengineer <nkmtear@yahoo.co.jp>
2020-09-16 15:02:57 +0900

    コード修正

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,16 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# article: http://ogp.me/ns/article#">
+　 <!-- Tracking code -->
+  <script>
+  　window.dataLayer = window.dataLayer || [];
+  　function gtag()　{
+    　dataLayer.push(arguments);
+　　}
+  　gtag('js', new Date());
+
+  　gtag('config', 'UA-178135632-1');
+　</script>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -21,32 +31,6 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
   <!-- LightBox -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.7.1/js/lightbox.min.js" type="text/javascript"></script>
-
-  <!-- Tracking code -->
-  <script>
-  　window.dataLayer = window.dataLayer || [];
-  　function gtag()　{
-    　dataLayer.push(arguments);
-　　}
-  　gtag('js', new Date());
-
-  　gtag('config', 'UA-178135632-1');
-　</script>
-
-  <!-- Google Tag Manager -->
-  <script>
-    (function(w,d,s,l,i) {
-      w[l]=w[l]||[];w[l].push({
-        'gtm.start': new Date().getTime(),
-         event: 'gtm.js'
-      });
-
-      var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })
-  (window,document,'script','dataLayer','GTM-XXXX');
-  </script>
 
   <!-- Current Path -->
   <script>

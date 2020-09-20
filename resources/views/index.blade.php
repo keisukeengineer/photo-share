4286f61bf7edae4b0fe1bcb663684702e59ddd28
keisukeengineer <nkmtear@yahoo.co.jp>
2020-09-20 23:04:13 +0900

    コード修正

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,19 +2,21 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# article: http://ogp.me/ns/article#">
   <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Photo Share</title>
   <meta name="keyword" content="写真、共有">
   <meta name="description" content="当サイトのサービスを利用すれば、お気に入りの写真を共有できます。">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <meta name="twitter:card" content="summary_large_image">
   <meta property="og:title" content="Photo Share" />
   <meta property="og:type" content="website" />
   <meta property="og:url" content="{{ url('/') }}/" />
   <meta property="og:image" content="{{ url('/') }}/photo-share-top.png" />
+  <meta property="og:site_name" content="{{ url('/') }}/">
   <meta property="og:description" content="当サイトのサービスを利用すれば、お気に入りの写真を共有できます。" />
   <meta property="og:locale" content="ja_JP" />
-
-  <title>Photo Share</title>
+  <!-- Twitter Cards の設定 -->
+  <meta name="twitter:card" content="summary_large_image">
+  <meta name="twitter:site" content="{{ url('/') }}/">
 
   <!-- Global site tag (gtag.js) - Google Analytics -->
   <script async src="https://www.googletagmanager.com/gtag/js?id=UA-178225654-1"></script>
@@ -26,16 +28,16 @@
     gtag('config', 'UA-178225654-1');
   </script>
   <!-- app.js -->
-  <script type="text/javascript" src="{{ mix('js/app.js') }}" defer></script>
+  <script src="{{ mix('js/app.js') }}" defer></script>
   <!-- FontAwesome -->
-  <script type="text/javascript" src="https://kit.fontawesome.com/61fdc2f5c9.js" crossorigin="anonymous"></script>
+  <script src="https://kit.fontawesome.com/61fdc2f5c9.js" crossorigin="anonymous"></script>
   <!-- jQuery -->
-  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
   <!-- LightBox -->
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.7.1/js/lightbox.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.7.1/js/lightbox.min.js"></script>
 
   <!-- Current Path -->
-  <script type="text/javascript">
+  <script>
     window.Laravel = {
       basePath: '{{ url('/') }}/'
     }
@@ -43,9 +45,9 @@
 
   <!-- app.css -->
   @if(app('env') == 'production')
-  <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
+  <link rel="stylesheet" href="{{ secure_asset('css/app.css') }}">
   @else
-  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}" rel="stylesheet">
   @endif
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://unpkg.com/ionicons@4.2.2/dist/css/ionicons.min.css">
